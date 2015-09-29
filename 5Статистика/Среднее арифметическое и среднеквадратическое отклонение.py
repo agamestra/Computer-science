fe06bb9b5ac05453c6f1254a2ec529ccd8da6c80
Evgeny <evgeny97.ru@gmail.com>
2015-09-29 11:49:45 +0300

    Computer science

--- 5Статистика/Среднее арифметическое и среднеквадратическое отклонение.py
+++ 5Статистика/Среднее арифметическое и среднеквадратическое отклонение.py
@@ -2,14 +2,30 @@ input = open('/home/student/PycharmProjects/untitled/4Генерация слу�
 summa = 0
 sumkvad = 0
 n = 0
+nmax = -1
+nmin = 101
+imax = 0
+imin = 0
 for i in range(int(150)):
-    a = input.readline()
-    summa += float(a)
-    sumkvad += float(a)**2
+    a = float(input.readline())
+    summa += a
+    sumkvad += a**2
     n += 1
+    if a > nmax:
+        nmax = a
+        imax = i
+    if a < nmin:
+        nmin = a
+        imin = i
 print(summa)
 print(sumkvad)
 print(n)
 print()
 print(summa/n)
 print(((sumkvad/n)-(summa/n)**2)**0.5)
+print()
+print(nmax)
+print(imax)
+print()
+print(nmin)
+print(imin)
